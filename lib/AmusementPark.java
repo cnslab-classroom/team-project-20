f997944335f15b1acf2c6f6490c95b21a3041dc6
juho-creator <juhokim2018@gmail.com>
2024-11-03 08:18:14 +0900

    [Fix] AmusementPark에 대기시간을 인스턴스 변수로 추가함

--- lib/AmusementPark.java
+++ lib/AmusementPark.java
@@ -4,6 +4,7 @@ package lib;
 public class AmusementPark {
     private String name;
     private String location; 
+    private int wait_time;
 
     public AmusementPark(String name, String location) {
         this.name = name;
