28b5bd14429136b2b927464a945f2e6f23bbde7f
juho-creator <juhokim2018@gmail.com>
2024-11-03 09:14:37 +0900

    [Feat] Ride 클래스에 getter 추가함

--- lib/Ride.java
+++ lib/Ride.java
@@ -4,12 +4,39 @@ public class Ride {
     
     private String rideName;
     private String rideType;
+    private int wait_time;
+    private int location; 
 
-    public Ride(String rideName, String rideType) {
+    public Ride(String rideName, String rideType, int wait_time, int location) {
         this.rideName = rideName;
         this.rideType = rideType;
+        this.wait_time = wait_time;
+        this.location = location;
     }
+    
     public void displayRideInfo() {
-        System.out.println("Ride Name: " + rideName + " | Ride Type: " + rideType);
+        System.out.println("Ride Name: " + rideName + " | Ride Type: " + rideType
+        + " | wait time: " + wait_time + " | location: " + location );
     }
+
+
+    public String getRideName(){
+        return rideName;
+    };
+
+    public int getRideType(){
+        return rideType;
+    };
+
+    public int getWaitTime(){
+        return wait_time;
+    };
+    
+    public int getLocation(){
+        return location;
+    }; 
+
 }
+
+
+
